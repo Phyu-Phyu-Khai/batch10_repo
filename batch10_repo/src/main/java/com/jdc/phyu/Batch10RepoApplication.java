451c719bb7f5693cdc941ff497725031e4387991
Phyu <phyukhai92@gmail.com>
2024-12-20 11:30:17 +0630

    init proj

---
+++ batch10_repo/src/main/java/com/jdc/phyu/Batch10RepoApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.phyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Batch10RepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Batch10RepoApplication.class, args);
+	}
+
+}
---
+++ batch10_repo/src/test/java/com/jdc/phyu/Batch10RepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdc.phyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Batch10RepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
